20ede83710d6bdf4c51bad6de0638cf95e7827ce
GencoMattia <gencomattia@hotmail.it>
2024-06-28 18:34:00 +0200

    added array coseDaFare

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,14 +2,83 @@
 export default {
     data() {
         return {
-        
+            coseDaFare: [
+                { 
+                    nome: "Lavare i piatti", 
+                    priorità: 3 
+                },
+                { 
+                    nome: "Sistemare camera", 
+                    priorità: 0 
+                },
+                { 
+                    nome: "Ordinare i cassetti", 
+                    priorità: 1 
+                },
+                { 
+                    nome: "Ordinare l'armadio", 
+                    priorità: 1 
+                },
+                { 
+                    nome: "Svuotare le ultime scatole e sistemare la scrivania", 
+                    priorità: 0 
+                },
+                { 
+                    nome: "Sistemare gli scaffali", 
+                    priorità: 1 
+                },
+                { 
+                    nome: "Buttare la spazzatura", 
+                    priorità: 3 
+                },
+                { 
+                    nome: "Pulire la cucina", 
+                    priorità: 2 
+                },
+                { 
+                    nome: "Pulire e sistemare il bagno", 
+                    priorità: 1 
+                },
+                { 
+                    nome: "Passare l'antiscarafaggi", 
+                    priorità: 0 
+                },
+                { 
+                    nome: "Pulire i davanzali delle finestre/i bordi della portafinestra da ragnatele e schifo", 
+                    priorità: 0 
+                },
+                { 
+                    nome: "Mettere le trappole per scarafaggi sotto la cucina", 
+                    priorità: 0 
+                },
+                { 
+                    nome: "Passare l'aspirapolvere sotto la cucina e buttare via le trappole vecchie", 
+                    priorità: 0 
+                },
+                { 
+                    nome: "Fare la spesa", 
+                    priorità: 2 
+                },
+                { 
+                    nome: "Pulire i mobili della cucina e mettere in ordine le cose della casa (utensili che erano già qui)", 
+                    priorità: 3 
+                },
+                { 
+                    nome: "Togliere i vestiti dallo stendino e metterli a posto", 
+                    priorità: 2 
+                },
+            ]
         };
     }
 };
 </script>
 
 <template>
+    <main>
+        <section class="container">
 
+        </section>
+    </main>
 </template>
 
 <style scoped lang="scss">
