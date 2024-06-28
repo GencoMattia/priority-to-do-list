23e293fc9be19ad5403d8066800dd32cc5aa7d2b
GencoMattia <gencomattia@hotmail.it>
2024-06-28 18:25:20 +0200

    added footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -9,7 +9,15 @@ export default {
 </script>
 
 <template>
-
+    <footer>
+        <section class="container">
+            <div class="row">
+                <h3>
+                    Developed with love
+                </h3>
+            </div>
+        </section>
+    </footer>
 </template>
 
 <style scoped lang="scss">
